4bb4bcb698981864db2b45d0dd6d39d7c9c2d8e3
jannikklein47 <jannikklein0302@outlook.de>
2025-04-17 00:41:25 +0200

    Initial Commit

---
+++ BatterySyncApp/BatterySyncAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatterySyncAppApp.swift
+//  BatterySyncApp
+//
+//  Created by Jannik Klein on 17.04.25.
+//
+
+import SwiftUI
+
+@main
+struct BatterySyncAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
